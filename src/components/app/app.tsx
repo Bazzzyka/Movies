1b904d7e5fde3c2d332d63cad18766497281bc72
Iuliia <u.baukina@gmail.com>
2021-05-05 11:26:33 +0500

    List styles

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -4,12 +4,13 @@ import List from '../list';
 import HiddenMovies from '../hiddenMovies';
 import { DATA } from '../../data';
 import commonStyles from '../components.module.scss';
+import appStyles from './app.module.scss';
 
 const App = () => { 
   return (
-    <div className={commonStyles.column}>
+    <div className={`${commonStyles.column} ${appStyles.bodyContainer}`}>
       <Header />
-      <div className={commonStyles.row}>
+      <div className={`${commonStyles.row} ${appStyles.listContainer}`}>
         <HiddenMovies />
         <List moviesData={DATA} />
       </div>
