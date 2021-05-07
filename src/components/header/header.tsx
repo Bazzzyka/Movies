6301f837af5b6db680d9a8ed2cbe4dd51a685053
Iuliia <u.baukina@gmail.com>
2021-05-07 15:45:19 +0500

    Header icons

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { ReactComponent as Logo } from '../../icons/logo.svg';
+import { ReactComponent as Star } from '../../icons/star.svg';
 import styles from './header.module.scss';
+import commonStyles from '../components.module.scss';
 
 const Header = () => {
   return (
-    <h1 className={styles.header}>
-      Movies to watch, enjoy, relax and inspire
-    </h1>
+    <div className={`${commonStyles.row} ${styles.container}`}>
+      <Logo className={styles.logo} />
+      <h1 className={styles.header}>
+        Movies to watch, enjoy, relax and inspire
+      </h1>
+      <Star className={styles.favorites} />
+    </div>
   );
 };
 
